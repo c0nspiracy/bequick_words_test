a7546660df5b5e158b69e7b46a20114f11f40337
krp312 <krp312@gmail.com>
2016-02-02 12:00:17 -0500

    Changing arrays to hashes

--- processor.rb
+++ processor.rb
@@ -1,80 +1,68 @@
+require 'pry'
+
 class Processor
 
   def initialize(input_filename, output_filename)
     @dictionary = get_words_from_file(input_filename)
     @output_filename = output_filename
     @sequences = []
-    @pairs_array = []
+    @pairs_hash = Hash.new { |pairs_hash, sequence| pairs_hash[sequence] = [] }
   end
 
   def create_list
     create_sequence_word_pairs
-    remove_unwanted_duplicates
+    select_unique_sequences
     alphabetize_pairs_by_sequence
-    output_to_file
   end
 
   def get_words_from_file(input_filename)
     File.read(input_filename).strip.split
   end
 
-  def extract_sequences_from_word(word)
-    sequences = []
-
+  def all_sequences(word)
+    @sequences = []
     while word.length > 3
       four_letter_sequence = word[0..3]
-
-      sequences << four_letter_sequence
-
+      @sequences << four_letter_sequence
       word = word[1..-1]
     end
-
-    return sequences
+    return @sequences
   end
 
   def create_sequence_word_pairs
     @dictionary.each do |word|
-      extracted_sequences = extract_sequences_from_word(word)
-      extracted_sequences.each do |sequence|
-        pair = [sequence, word]
-        @pairs_array << pair
-      end
+      extracted_sequences = all_sequences(word)
+      extracted_sequences.each { |sequence| @pairs_hash[sequence] << word }
     end
   end
 
-  def output_to_file
-    File.open @output_filename, "w" do |csv|
-      @pairs_array.each do |pairing|
-        sequence = pairing[0]
-        original = pairing[1]
-        output_line = formatter sequence, original
-        csv.puts output_line
-      end
-    end
-  end
-
-  def identify_duplicate_sequences
-    @sequences.select { |e| @sequences.count(e) > 1 }.uniq
-  end
-
-  def remove_unwanted_duplicates
-    @pairs_array.map{ |pair| pair.first }
-    duplicate_sequences = identify_duplicate_sequences
-    @pairs_array.reject! do |sequence, original|
-      duplicate_sequences.include? sequence
+  def select_unique_sequences
+    @pairs_hash.select do |sequence, duplicate_words_array|
+      duplicate_words_array.length == 1
     end
   end
 
   def alphabetize_pairs_by_sequence
-    @pairs_array.sort_by!{ |sequence, original| sequence.downcase  }
+    select_unique_sequences.sort_by { |sequence, word| sequence.downcase }
   end
 
-  def formatter(sequence, word)
-    [quote_string(sequence), quote_string(word)].join '   '
-  end
-
-  def quote_string(s)
-    '' + s + ''
-  end
+  # def output_to_file
+  #   File.open @output_filename, "w" do |csv|
+  #     @pairs_array.each do |pairing|
+  #       sequence = pairing[0]
+  #       original = pairing[1]
+  #       output_line = formatter(sequence, original)
+  #       csv.puts output_line
+  #     end
+  #   end
+  # end
+  #
+  # def formatter(sequence, word)
+  #   [quote_string(sequence), quote_string(word)].join '   '
+  # end
+  #
+  # def quote_string(s)
+  #   '' + s + ''
+  # end
 
 end
